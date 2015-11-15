09565a4ac0dcfd6baf2b99fae13647c3dd1291a1
Brandon Keepers <brandon@opensoul.org>
2015-11-15 00:03:52 -0500

    Fix parens in image test

--- test/unused_images_test.rb
+++ test/unused_images_test.rb
@@ -4,7 +4,7 @@ describe "images" do
   Dir["images/**/*"].each do |path|
     it "references #{path}" do
       return unless File.file?(path)
-      system "grep -r -m 1 #{path} _site > /dev/null" || fail("#{path} is unused")
+      system("grep -rq #{path} _site") || fail("#{path} is unused")
     end
   end
 end
